c9f2a28af5c0e9c06a1a55626ad210a67d7e7d46
achilles-000 <stefanpopovic976@gmail.com>
2024-11-29 23:43:25 -0500

    base template for game

---
+++ main.c
@@ -0,0 +1,103 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // Initialize SDL_ttf
+    if (TTF_Init() == -1) {
+        fprintf(stderr, "SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
+        return 1;
+    }
+
+    // Create a window
+    SDL_Window *window = SDL_CreateWindow("Movable Window",
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          640, 480,
+                                          SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    if (window == NULL) {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    // Create renderer
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (!renderer) {
+        printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Main loop
+    SDL_bool running = SDL_TRUE;
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    running = SDL_FALSE;
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    if (event.button.button == SDL_BUTTON_LEFT) {
+                        // Save the initial window position
+                        int initialX, initialY;
+                        SDL_GetWindowPosition(window, &initialX, &initialY);
+
+                        // Move the window with the mouse
+                        int windowX, windowY;
+                        while (SDL_PollEvent(&event)) {
+                            if (event.type == SDL_MOUSEMOTION) {
+                                windowX = initialX + event.motion.x - event.button.x;
+                                windowY = initialY + event.motion.y - event.button.y;
+                                SDL_SetWindowPosition(window, windowX, windowY);
+                            } else if (event.type == SDL_MOUSEBUTTONUP &&
+                                       event.button.button == SDL_BUTTON_LEFT) {
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+            if (!renderer) {
+        printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+     SDL_SetRenderDrawColor(renderer, 0, 0 ,0, 255);
+     SDL_RenderClear(renderer);
+
+    // Set the draw color to red and draw a filled rectangle
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_Rect rect = {100, 100, 200, 150};
+    SDL_RenderFillRect(renderer, &rect);
+
+    // Update the screen
+    SDL_RenderPresent(renderer);
+
+
+
+
+    }
+
+    // Clean up
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    TTF_Quit();
+    SDL_Quit();
+    return 0;
+}
